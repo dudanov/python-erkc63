a89845d370154ded6f0533769438cd10fde66197
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2025-06-02 10:07:11 +0400

    changes

--- src/erkc63/parsers/accounts.py
+++ src/erkc63/parsers/accounts.py
@@ -8,6 +8,15 @@ from bs4.filter import SoupStrainer
 from ..types import AccountInfo
 from ..utils import str_normalize
 
+_POS_TYPES = tuple(
+    (pos, str(field.type))
+    for pos, field in zip(
+        [0, 1, 2, 3, 5, 7, 9, 11, 13, 14, 16, 18, 20, 22],  # позиции тегов
+        dc.fields(AccountInfo),  # поля
+        strict=True,
+    )
+)
+
 
 def parse_accounts(html: str) -> list[int]:
     """Парсит меню выбора лицевого счета"""
@@ -29,23 +38,18 @@ def parse_accounts(html: str) -> list[int]:
 def parse_account(html: str) -> AccountInfo:
     """Парсит главную страницу лицевого счета со сводной информацией"""
 
-    TAG_POSITIONS = 0, 1, 2, 3, 5, 7, 9, 11, 13, 14, 16, 18, 20, 22
-    INFO_FIELDS = dc.fields(AccountInfo)
-
-    assert len(TAG_POSITIONS) == len(INFO_FIELDS)
-
     x = SoupStrainer(
         name="div",
         class_=lambda k: k is not None and k.find("text-col-") != -1,
     )
 
-    contents = BeautifulSoup(html, "lxml", parse_only=x).contents
+    tags = BeautifulSoup(html, "lxml", parse_only=x).contents
 
     def _args() -> Iterator[Any]:
-        for pos, field in zip(TAG_POSITIONS, INFO_FIELDS):
-            string = next(contents[pos].stripped_strings)
+        for pos, _type in _POS_TYPES:
+            string = next(tags[pos].stripped_strings)
 
-            match str(field.type):
+            match _type:
                 case "str":
                     yield str_normalize(string)
 
