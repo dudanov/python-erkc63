e8b383ea6c4485073c56b5ce8b768b95f05db993
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2025-06-02 13:23:00 +0400

    change

--- src/erkc63/parsers/parser.py
+++ src/erkc63/parsers/parser.py
@@ -4,12 +4,12 @@ from bs4 import BeautifulSoup, Tag
 from bs4.filter import SoupStrainer
 
 
-def parse_html_divclass(html: str, class_: str) -> list[Tag]:
+def parse_html_divclass(html: str, cls: str) -> list[Tag]:
     """Возвращает список тегов `div` указанного класса"""
 
     x = SoupStrainer(
         name="div",
-        class_=lambda x: x is not None and x.find(class_) != -1,
+        class_=lambda x: x is not None and x.find(cls) != -1,
     )
 
     return cast(list[Tag], BeautifulSoup(html, "lxml", parse_only=x).contents)
