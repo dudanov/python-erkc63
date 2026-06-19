4b5f5701bcc55598ec67f0a7aca88b670991a4a3
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2026-06-19 16:51:26 +0400

    change

--- src/erkc63/parsers/qrcode.py
+++ src/erkc63/parsers/qrcode.py
@@ -1,13 +1,16 @@
+import dataclasses as dc
 import io
-from typing import Literal
 
 import pymupdf
 from PIL import Image
 
 type PilImage = Image.Image
 
-type PdfSupported = Literal["erkc", "peni"]
-type QrSupported = Literal["erkc", "kapremont", "peni"]
+
+@dc.dataclass(slots=True)
+class AccrualImages:
+    page: bytes
+    codes: list[bytes]
 
 
 def pix_save(pix: pymupdf.Pixmap, max_rect: tuple[int, int]) -> bytes:
@@ -64,70 +67,23 @@ def page_png(page: pymupdf.Page, max_rect: tuple[int, int]) -> bytes:
     return pix_save(pix, max_rect)
 
 
-class QrCodes:
-    _pdf_data: dict[PdfSupported, bytes]
-    _qr_png: dict[QrSupported, bytes]
+# 'img2', 'img4' - основной счет
+# 'img0' - счет пени
+def accrual_images(
+    data: bytes | None,
+    images: list[str],
+    max_rect: tuple[int, int] = (3840, 2160),
+):
+    if not data:
+        return
 
-    def __init__(
-        self,
-        pdf_erkc: bytes | None,
-        pdf_peni: bytes | None,
-        *,
-        max_rect: tuple[int, int] = (3840, 2160),
-    ):
-        if not all(x > 0 for x in max_rect):
-            raise ValueError("Ограничения max_rect должны быть больше нуля.")
+    if not all(x > 0 for x in max_rect):
+        raise ValueError("Ограничения max_rect должны быть больше нуля.")
 
-        self._pdf_data = {}
-        self._qr_png = {}
+    with pymupdf.open(stream=data) as doc:
+        page = doc[0]
 
-        if pdf_erkc:
-            with pymupdf.open(stream=pdf_erkc) as doc:
-                page = doc[0]
-                self._pdf_data["erkc"] = page_png(page, max_rect)
-                self._qr_png["erkc"] = qr_png(page, "img2", max_rect)
-                self._qr_png["kapremont"] = qr_png(page, "img4", max_rect)
-
-        if pdf_peni:
-            with pymupdf.open(stream=pdf_peni) as doc:
-                page = doc[0]
-                self._pdf_data["peni"] = page_png(page, max_rect)
-                self._qr_png["peni"] = qr_png(page, "img0", max_rect)
-
-    def _qr(self, qr: QrSupported) -> bytes | None:
-        return self._qr_png.get(qr)
-
-    def _pdf(self, pdf: PdfSupported) -> bytes | None:
-        """Возвращает указанный счет в формате `PNG`."""
-
-        return self._pdf_data.get(pdf)
-
-    @property
-    def qr_erkc(self) -> bytes | None:
-        """QR-код оплаты коммунальных услуг."""
-
-        return self._qr("erkc")
-
-    @property
-    def qr_kapremont(self) -> bytes | None:
-        """QR-код оплаты капитального ремонта."""
-
-        return self._qr("kapremont")
-
-    @property
-    def qr_peni(self) -> bytes | None:
-        """QR-код оплаты пени."""
-
-        return self._qr("peni")
-
-    @property
-    def pdf_erkc(self) -> bytes | None:
-        """Счет основной в формате `PNG`."""
-
-        return self._pdf("erkc")
-
-    @property
-    def pdf_peni(self) -> bytes | None:
-        """Счет на пени в формате `PNG`."""
-
-        return self._pdf("peni")
+        return AccrualImages(
+            page=page_png(page, max_rect),
+            codes=[qr_png(page, x, max_rect) for x in images],
+        )
