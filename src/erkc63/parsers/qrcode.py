c326ddeb50906b844f09a6742b066dd6637fbcf9
Sergey Dudanov <sergey.dudanov@gmail.com>
2026-06-19 19:32:20 +0400

    change

--- src/erkc63/parsers/qrcode.py
+++ src/erkc63/parsers/qrcode.py
@@ -13,52 +13,45 @@ class AccrualImages:
     codes: list[bytes]
 
 
-def pix_save(pix: pymupdf.Pixmap, max_rect: tuple[int, int]) -> bytes:
+def pix_save(pix: pymupdf.Pixmap, rect: tuple[int, int]) -> bytes:
     """Сохраняет Pixmap в 8-битный оптимизированный `PNG` с палитрой `WEB`."""
 
     bio = io.BytesIO()
 
     img = pix.pil_image()
-    img.thumbnail(max_rect)
+    img.thumbnail(rect)
     img = img.convert(mode="P", palette=Image.Palette.WEB)
     img.save(bio, format="png", optimize=True)
 
     return bio.getvalue()
 
 
-def qr_png(
-    page: pymupdf.Page,
-    img_name: str,
-    max_rect: tuple[int, int],
-) -> bytes:
+def img_png(page: pymupdf.Page, name: str, rect: tuple[int, int]) -> bytes:
     """Извлекает изображение со страницы `PDF` в данные `PNG` формата."""
 
-    if not img_name:
-        raise ValueError("Имя изображения не может быть пустой строкой.")
+    for item in page.get_images():
+        img_xref, img_name = item[0], item[7]
 
-    for img_info in page.get_images():
-        xref, name = img_info[0], img_info[7]
-
-        if name != img_name:
+        if img_name != name:
             continue
 
-        pix = pymupdf.Pixmap(page.parent, xref)
+        pix = pymupdf.Pixmap(page.parent, img_xref)
 
-        return pix_save(pix, max_rect)
+        return pix_save(pix, rect)
 
-    raise FileNotFoundError("QR код на странице PDF не найден.")
+    raise FileNotFoundError("Изображение на странице не найдено.")
 
 
-def page_png(page: pymupdf.Page, max_rect: tuple[int, int]) -> bytes:
+def page_png(page: pymupdf.Page, rect: tuple[int, int]) -> bytes:
     """
     Рендерит страницу `PDF` в данные изображения `PNG` формата.
     Итоговый размер пропорционально вписывается в указанные ограничения, по умолчанию 4К (3840 x 2160).
     """
 
-    factor = min(x / y for x, y in zip(max_rect, page.rect[2:]))
+    factor = min(x / y for x, y in zip(rect, page.rect[2:]))
     pix = page.get_pixmap(matrix=pymupdf.Matrix(factor, factor))
 
-    return pix_save(pix, max_rect)
+    return pix_save(pix, rect)
 
 
 # 'img2', 'img4' - основной счет
@@ -66,18 +59,18 @@ def page_png(page: pymupdf.Page, max_rect: tuple[int, int]) -> bytes:
 def accrual_images(
     data: bytes | None,
     images: list[str],
-    max_rect: tuple[int, int] = (3840, 2160),
+    rect: tuple[int, int] = (3840, 2160),
 ):
     if not data:
         return
 
-    if not all(x > 0 for x in max_rect):
+    if not all(x > 0 for x in rect):
         raise ValueError("Ограничения max_rect должны быть больше нуля.")
 
     with pymupdf.open(stream=data) as doc:
         page = doc[0]
 
         return AccrualImages(
-            page=page_png(page, max_rect),
-            codes=[qr_png(page, x, max_rect) for x in images],
+            page=page_png(page, rect),
+            codes=[img_png(page, x, rect) for x in images],
         )
